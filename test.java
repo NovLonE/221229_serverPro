6891c20210d47765a500ad2cb6a5a3879ab25f51
NovLonE <chldbftkd@gmail.com>
2022-12-29 11:27:26 +0900

    first commit

---
+++ test.java
@@ -0,0 +1,17 @@
+package test1229;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		String print = "221229_서버프로그램 구현 : 최율상";
+		
+		char[] printArr = print.toCharArray();
+		
+		for(int i = 0 ; i<printArr.length ; i++) {
+			System.out.print(printArr[i]);
+		}
+
+	}
+
+}
